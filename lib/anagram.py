4f366f97372021df4913a029ee6ea380c0a822ad
Enkhbat Munkhbold <emunkhbold10@gmail.com>
2024-08-18 19:09:04 -0700

    Complete lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,7 @@
-# your code goes here!
+class Anagram:
+  def __init__(self, word):
+    self.word = word
+
+  # @classmethod
+  def match(self, words):
+    return [word for word in words if sorted(word) == sorted(self.word)]
